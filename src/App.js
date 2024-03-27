860bba9481eb93a4d957f01a9cbfbc190cf59eb5
David-M-M-Isaac <David-M-M-Isaac>
2024-03-27 18:01:18 +0000

    footer change v1

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Footer from './components/Footer.jsx';
 import House from './components/House.jsx';
 import CaixaLivro from "./components/caixaLivro";
 import Books from "./db.json";
+import Pesquisa from './components/barra_pesquisa.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Pesquisa />
         <CaixaLivro
             livro={Books.books[0]}
         />
