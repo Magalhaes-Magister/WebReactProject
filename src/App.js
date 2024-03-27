e06e0df955e1263916e2c85bf204aee3964cc69b
iagms <iagms@iscte-iul.pt>
2024-03-27 16:54:47 +0000

    Adição da caixaLivro.js e das imagens de estrelas, como também o ficheiro json dos livros.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import House from './components/House.jsx';
+import CaixaLivro from "./components/caixaLivro";
+import Books from "./db.json";
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <CaixaLivro
+            livro={Books.books[0]}
+        />
       <House />
       <Footer /> 
     </div>
---
+++ src/components/caixaLivro.js
@@ -0,0 +1,44 @@
+import './style_caixaLivro.css'
+import star_1 from '../imagens/1_stars.png'
+import star_2 from '../imagens/2_stars.png'
+import star_3 from '../imagens/3_stars.png'
+import star_4 from '../imagens/4_stars.png'
+import star_5 from '../imagens/5_stars.png'
+export default function CaixaLivro({livro}){
+
+    let titulo = livro.title;
+    let autores = livro.authors;
+    let thumbnail = livro.thumbnailUrl;
+    let estrelas;
+    switch (livro.score){
+        case 1:
+            estrelas = star_1;
+            break;
+        case 2:
+            estrelas = star_2;
+            break;
+        case 3:
+            estrelas = star_3;
+            break;
+        case 4:
+            estrelas = star_4;
+            break;
+        case 5:
+            estrelas = star_5;
+            break;
+    }
+
+    return(
+        <>
+            <div className={"caixa"}>
+                <img src={thumbnail} width={155}/>
+                <div className="info">
+                    <strong>{titulo}</strong>
+                    <p>{autores}</p>
+                    <img className={"rating"} src={estrelas} width={150}/>
+                </div>
+                <button>Comprar</button>
+            </div>
+        </>
+    )
+}
