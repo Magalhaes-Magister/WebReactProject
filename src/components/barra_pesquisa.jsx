05a482828a32ac52f67b03ffb6cbbb04c51fc68a
David-M-M-Isaac <David-M-M-Isaac>
2024-03-27 16:59:24 +0000

    header v3

--- src/components/barra_pesquisa.jsx
+++ src/components/barra_pesquisa.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import './barra_pesquisa.css'; 
 
 function Pesquisa() {
-  return (
-    <div> 
-        
-    </div>
-  );
+    return (
+        <div id="barra-pesquisa">
+            <div className="pesquisa">
+                <i className="bi bi-search"></i>
+                <input type="text" placeholder="Input" />
+            </div>
+            <div className="categorias">
+                <select>
+                    <option value="categoria1">Autor</option>
+                    <option value="categoria2">Titulo</option>
+                    <option value="categoria3">Categoria</option>
+                </select>
+            </div>
+        </div>
+    );
 }
 
 export default Pesquisa;
