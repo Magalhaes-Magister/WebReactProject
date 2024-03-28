981f660b035f6e235cdc5bca205ccf9e24888293
David-M-M-Isaac <David-M-M-Isaac>
2024-03-28 13:11:04 +0000

    botao filtro v2

--- src/components/Filtro.jsx
+++ src/components/Filtro.jsx
@@ -4,29 +4,29 @@ import './Filtro.css';
 function filtro() {
     return (
         <div className="filtro">
-            <li className="botao_filtro">
-                <span className="Opcoes">Ordenar Por</span>
-                <ul className='Zero'>
-                    <li className='Primeiro'>
-                        <a href="">Ranking</a>
-                        <ul id='Primeiro_1'>
-                            <li><a href="">0-1</a></li>
-                            <li><a href="">1-2</a></li>
-                            <li><a href="">2-3</a></li>
-                            <li><a href="">3-4</a></li>
-                            <li><a href="">4-5</a></li>
-                        </ul>
-                    </li>
-                    <li className='Segundo'>
-                        <a href="">Preço</a>
-                        <ul id='Segundo_2'>
-                            <li><a href=""> 0-10 </a></li>
-                            <li><a href=""> 10-20 </a></li>
-                            <li><a href=""> 20-30 </a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+            <div className="Botao_filtro">
+                <button className="Botao_Principal">Ordenar Por</button>
+                <div className="Botao_drop">
+                    <div className="Botao_segundario">
+                        <a href="#">Ranking</a>
+                        <div className="Botao_segundario_conteudo">
+                            <a href="#">0-1</a>
+                            <a href="#">1-2</a>
+                            <a href="#">2-3</a>
+                            <a href="#">3-4</a>
+                            <a href="#">4-5</a>
+                        </div>
+                    </div>
+                    <div className="Botao_segundario">
+                        <a href="#">Preço</a>
+                        <div className="Botao_segundario_conteudo">
+                            <a href="#">0-10</a>
+                            <a href="#">10-20</a>
+                            <a href="#">20-30</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
