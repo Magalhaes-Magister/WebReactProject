8c0459163870487959f4ea53d01e61db9dbd37a4
David-M-M-Isaac <David-M-M-Isaac>
2024-03-28 13:28:54 +0000

    criação pagina principal e app v1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import House from './components/House.jsx';
-import CaixaLivro from "./components/caixaLivro";
-import CaixaCaixaLivro from "./components/caixaCaixaLivro";
-import Books from "./db.json";
-import Pesquisa from './components/barra_pesquisa.jsx';
-import Barra_numero from './components/barra_numeros.jsx';
-import Filtro from './components/Filtro.jsx';
+import Pagina_Principal from './Pagina_Principal.js';
 
 function App() {
   return (
     <>
-      <div className="App">
-        <Header />
-        <div className="Pesquisa">
-          <Pesquisa style="" />
-        </div>
-        <div className="Filtro">
-        <Filtro />
-      </div>
-        <div className="CaixaCaixaLivro"></div>
-        <CaixaCaixaLivro />
-      </div>
-      <div className="Barra_numero">
-        <Barra_numero />
-      </div>
-      <House />
-      <Footer />
+      <Pagina_Principal />
     </>
   );
 }
---
+++ src/Pagina_Principal.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Pagina_Principal.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import House from './components/House.jsx';
+import CaixaLivro from "./components/caixaLivro.js";
+import CaixaCaixaLivro from "./components/caixaCaixaLivro.jsx";
+import Books from "./db.json";
+import Pesquisa from './components/barra_pesquisa.jsx';
+import Barra_numero from './components/barra_numeros.jsx';
+import Filtro from './components/Filtro.jsx';
+
+function Pagina_Principal() {
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <div className="Pesquisa">
+          <Pesquisa style="" />
+        </div>
+        <div className="Filtro">
+        <Filtro />
+      </div>
+        <div className="CaixaCaixaLivro"></div>
+        <CaixaCaixaLivro />
+      </div>
+      <div className="Barra_numero">
+        <Barra_numero />
+      </div>
+      <House />
+      <Footer />
+    </>
+  );
+}
+
+export default Pagina_Principal;
