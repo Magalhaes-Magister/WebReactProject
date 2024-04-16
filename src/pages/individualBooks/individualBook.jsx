0b5877b5b45393ed7a62841c25509c407780f0f3
iagms <iagms@iscte-iul.pt>
2024-04-16 10:42:48 +0100

    feat: finalização da página de livro individual

--- src/pages/individualBooks/individualBook.jsx
+++ src/pages/individualBooks/individualBook.jsx
@@ -1,25 +1,108 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { useParams } from 'react-router-dom';
 import {BOOKS} from "../../books";
+import style from "./individualBook.module.css";
+import Button from "react-bootstrap/Button";
+import star_1 from "../../images/1_stars.png";
+import star_2 from "../../images/2_stars.png";
+import star_3 from "../../images/3_stars.png";
+import star_4 from "../../images/4_stars.png";
+import star_5 from "../../images/5_stars.png";
+import missing from "../../images/missing_img.png";
+import Styles from "../../components/book_style.module.css";
+import basket from "../../images/basket.png";
+import {ShopContext} from "../../context/shop-context";
+
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+}
+
+function formatDate(date) {
+    return [
+        padTo2Digits(date.getDate()),
+        padTo2Digits(date.getMonth() + 1),
+        date.getFullYear(),
+    ].join('/');
+}
 
 export const IndividualBook = () => {
     const { livroId } = useParams();
-    const {thumbnailUrl, longDescription, title, authors, publishedDate, categories, pageCount, score, price} = BOOKS.find((item) => { return item.id === livroId});
+    const {thumbnailUrl, longDescription, title, authors, publishedDate, categories, pageCount, score, price, isbn, status}
+        = BOOKS.find((item) => { return item.id === livroId});
+    const {addToCart} = useContext(ShopContext);
+    let preco = price || "Indisponível";
+    let thumbnail = thumbnailUrl || missing;
+
+    let estrelas;
+    switch (score){
+        case 1:
+            estrelas = star_1;
+            break;
+        case 2:
+            estrelas = star_2;
+            break;
+        case 3:
+            estrelas = star_3;
+            break;
+        case 4:
+            estrelas = star_4;
+            break;
+        case 5:
+            estrelas = star_5;
+            break;
+    }
+
 
     return (
-        <div className={"livro_caixa"}>
-            <img src={thumbnailUrl}/>
-            <div className={"caixa_info"}>
-                <p>{title}</p>
-                <ul>
-                    {authors.map(autor => {return (<li>{autor}</li>)})}
-                </ul>
-                <p>{longDescription}</p>
-                <p>Publicado em {publishedDate.$date}</p>
-                <p>Num de páginas: {pageCount}</p>
-                <p>Score: {score}/5</p>
-                <p>Preço: {price}</p>
+        <div className={style.livroCaixa}>
+
+            <div className={style.livroImg}>
+                <img src={thumbnail} className={style.thumb}
+                    onError={event => {
+                    event.target.src = missing
+                    event.onerror = null
+                }}/>
             </div>
+
+            <div className={style.livroLeft}>
+                <div className={style.livroInfo}>
+                    <h1 style={{fontWeight: "bold"}}>{title}</h1>
+                    <ul style={{listStyleType: "none"}}>
+                        by
+                        {authors.map(autor => {
+                            return (<li>{autor}</li>)
+                        })}
+                    </ul>
+                    <img src={estrelas} style={{maxWidth: "30%", minWidth: "170px"}}/>
+                    <p className={style.preco}>{preco} {preco!=="Indisponível"? "€":""}</p>
+                    {preco !== "Indisponível" ? (
+                        <Button variant="warning" style={{width: "45%", height: "50px"}} onClick={() => addToCart(livroId)}>
+                            <img src={basket} alt="" width={25}/>
+                            &ensp;
+                            Comprar
+                        </Button>): (
+                        <Button variant="warning" style={{width: "45%", height: "50px"}}  disabled>
+                            <img src={basket} alt="" width={25}/>
+                            &ensp;
+                            Comprar
+                        </Button>
+                    )}
+                </div>
+                <div>
+                    {longDescription != null ? (
+                        <>
+                            <h2>Sinopse</h2>
+                            <p className={style.paddLeft}>{longDescription}</p>
+                        </>
+                    ) : (<></>)}
+                    <h3>Características</h3>
+                    <p className={style.paddLeft}>Publicado em {formatDate(new Date(publishedDate.$date))}</p>
+                    <p className={style.paddLeft}>Num de páginas: {pageCount}</p>
+                    <p className={style.paddLeft}>isbn: {isbn}</p>
+
+                </div>
+            </div>
+
         </div>
     )
 }
