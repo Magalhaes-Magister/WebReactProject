cd5385678b9cffa33e584910f2a2f26729c32b4a
iagms <iagms@iscte-iul.pt>
2024-05-16 14:36:07 +0100

    Fix do Objectid

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask
 from pymongo import MongoClient
 from flask_cors import CORS
+from bson import ObjectId, json_util
 import json
 
 app = Flask(__name__)
@@ -9,8 +10,12 @@ CORS(app)
 client = MongoClient('mongodb+srv://iagms:uHj56fJdlWHTurVd@clusterprojeto.ca1xd8i.mongodb.net/',tls=True,tlsAllowInvalidCertificates=True)
 db = client["Projeto_livraria"]
 
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
 @app.route("/books", methods=["GET"])
 def get_books():
 
-    book = db.books.find()
-    return json.dumps(list(book), default=str)
+    books = db.books.find()
+    books_list = list(books)
+    return parse_json(books_list), 200
